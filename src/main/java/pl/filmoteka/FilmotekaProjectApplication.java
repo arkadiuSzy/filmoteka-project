856061e39ee96697c4f269b15328ede3c5aedfcd
arkadiuSz77 <arkadiuSz77@wp.pl>
2023-06-30 20:13:17 +0200

    initial commit

---
+++ src/main/java/pl/filmoteka/FilmotekaProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.filmoteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotekaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmotekaProjectApplication.class, args);
+    }
+
+}
